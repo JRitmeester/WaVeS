0ac4d11de359360084ff049e1c09cbc2173f0a3c
Jeroen Ritmeester <jeroen.c.ritmeester@gmail.com>
2025-03-18 10:29:57 +0100

    Fix build file

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 pyinstaller --onefile --noconsole --specpath WaVeS/spec --distpath WaVeS/dist --workpath WaVeS/build --hidden-import win32api main.py
-copy "WaVeS/dist" "."
+copy "WaVeS\dist\*" "."
 @RD /S /Q "WaVeS"
